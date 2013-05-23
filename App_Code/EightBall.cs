46802f065941fa630fe9876c2138e5058be46f5d
marionkdit <marionkdit@gmail.com>
2013-05-23 11:15:39 -0700

    Initial commit

---
+++ App_Code/EightBall.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+[ServiceContract(Namespace = "")]
+[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+public class EightBall
+{
+	// To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+	// To create an operation that returns XML,
+	//     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+	//     and include the following line in the operation body:
+	//         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+
+        [OperationContract]
+        public string getQuote(int i){
+            string[] theQuotes = {"Yesterday is gone, tomorrow is not yet here, we only have today, so let us begin.  -Mother Teresa", "Great spirits have always encountered violent opposition from mediocre minds. -Albert Einstein", "Great minds discuss ideas; average minds discuss events; small minds discuss people.  -Eleanor Roosevelt", "There is no greater agony than bearing an untold story inside you.", "The medocre teacher tells.  The good teacher explains.  The superior teacher demonstrates.  The great teacher inspires.  -William Arthur Ward"};
+            string quotation = theQuotes[i];
+		    return quotation;
+        }
+	}
+
+	// Add more operations here and mark them with [OperationContract]
+
---
+++ Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+        <Services>
+            <asp:ServiceReference Path="~/EightBall.svc" />
+        </Services>
+    </asp:ScriptManager>
+
+    <input type="text" id="txtInput" />
+    <p>Please enter a number between 0 and 4.</p>
+    <input type="button" id="btnGo" value="Shake the ball!" onclick="ShakeIt();" />
+    <p id="theResult"></p>
+
+    <script type="text/javascript">
+        function ShakeIt() {
+            var i = document.getElementById('txtInput').value;
+            
+            EightBall.getQuote(i, TestServiceCallBack(), TestServiceError());
+        }
+
+        function TestServiceCallBack(result) {
+            document.getElementById('theResult').innerHTML = result;
+
+        }
+
+        function TestServiceError() {
+           //document.getElementById('theResult').innerHTML = "Danger Will Robinson, Danger!";
+        }
+
+    </script>
+</asp:Content>
+
